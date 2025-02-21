4286516ac9b692c9f7bb3caeeebf5a46e3b6237e
zyedshamsuddin <zyedshamsuddin@gmail.com>
2025-02-21 10:31:17 +0000

    updated idx dev nix config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -48,10 +48,10 @@
     previews = {
       enable = true;
       previews = {
-        web = {
-          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-          manager = "flutter";
-        };
+        # web = {
+        #   command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+        #   manager = "flutter";
+        # };
         android = {
           command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
           manager = "flutter";
